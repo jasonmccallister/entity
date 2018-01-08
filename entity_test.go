26d042722217b1da60c00f7f0b929d9351610c1f
Jason McCallister <themccallister@gmail.com>
2018-01-08 02:04:58 -0500

    Create entity_test.go

---
+++ entity_test.go
@@ -0,0 +1,26 @@
+package entity_test
+
+import (
+	"os"
+	"testing"
+
+	_ "github.com/jinzhu/gorm/dialects/postgres"
+	_ "github.com/jinzhu/gorm/dialects/sqlite"
+	"github.com/traderinteractive/dct-entity/entity"
+)
+
+func TestCanGetAnInMemoryDatabaseBasedOnAppEnv(t *testing.T) {
+	os.Setenv("APP_ENV", "testing")
+
+	sess, err := entity.NewSession()
+	if err != nil {
+		t.Fatalf("expected the error to not be nil, got `%v` instead", err)
+	}
+
+	err = sess.ORM.DB().Ping()
+	if err != nil {
+		t.Fatal("we should have been able to ping the db in memory")
+	}
+
+	os.Setenv("APP_ENV", "")
+}
